4ca99d3ed27d3b46d96549b69a4406446b4ffe19
NilayaAgarwal <49634326+NilayaAgarwal@users.noreply.github.com>
2022-07-10 18:32:05 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/OlaconnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlaconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlaconnectApplication.class, args);
+	}
+
+}
